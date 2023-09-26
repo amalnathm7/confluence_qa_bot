475eeda5ccba27d5ff826f63cafa3df42463f96c
Amal Nath M <amalnathm7@gmail.com>
2023-09-26 18:31:24 +0530

    Feat: UI fixes

--- src/app.py
+++ src/app.py
@@ -13,13 +13,20 @@ st.title("Confluence Q&A")
 
 question = st.text_input('Ask a question', "Hi, who are you?")
 
-get_answer_button = st.button('Get Answer', key="get_answer_button")
+BUTTON_RUNNING = False
+
+get_answer_button = st.button('Get Answer')
 
 if get_answer_button:
     with st.spinner("Running..."):
         answer = confluence_qa_bot.run(question)
 
+    BUTTON_RUNNING = False
+
     st.success('Done!')
     st.write(answer)
 else:
-    st.text("Click 'Get Answer' to retrieve the answer")
+    if not BUTTON_RUNNING:
+        st.text("Click 'Get Answer' to retrieve the answer")
+    else:
+        st.button('Get Answer', key="get_answer_button", disabled=True)
